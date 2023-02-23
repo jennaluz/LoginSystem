7701c2467201de8e68109a259af04592dd219767
jennaluz <jpura827@gmail.com>
2023-02-22 23:43:54 -0800

    Updated database.

--- database/Users.sql
+++ database/Users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 23, 2023 at 04:08 AM
+-- Generation Time: Feb 23, 2023 at 08:41 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -36,6 +36,17 @@ CREATE TABLE `Users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`UserID`, `FirstName`, `LastName`, `EmailAddress`, `Password`) VALUES
+(5, 'Marshal', 'Mathers', 'enimem@gmail.com', '93d2f1dce3ee89ce5b05e9e057ac925e'),
+(6, 'Pam', 'Beesley', 'beesleyp@gmail.com', '0ff52181eb2b7152415bd9e47e01fcd4'),
+(8, 'Jim', 'Halpert', 'halpertj@gmail.com', 'eef3a79f83c3fcf4e739b8ac76e591b1'),
+(9, 'Stanley', 'Hudson', 'hudsons@gmail.com', 'c7656ce3fbb462c82bad4e11fc7f4165'),
+(11, 'Jenna', 'Lindsay', 'lindsayj@gmail.com', '38f1706725a4b22bc354f595eceb0a64');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -54,7 +65,7 @@ ALTER TABLE `Users`
 -- AUTO_INCREMENT for table `Users`
 --
 ALTER TABLE `Users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
